d815643d572113858302856704e86907317f2cc4
BuildTools <unconfigured@null.spigotmc.org>
2022-01-24 14:31:00 +0100

    exo4 correct

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 //const helloWorld = React.createElement('h1', {}, 'Hello world!');
 
-const firstName = 'r0ulito';
-const lastName = 'formateur';
+const firstName = 'Jérémie';
+const lastName = 'Bomont';
 
 
 // Solution sans bonus
-const helloWorld = <h1>Hello <span>{firstName[0].toUpperCase() + firstName.substr(1)}</span> <span className="red-text">{lastName.toUpperCase()}</span></h1>;
 
-ReactDOM.render(helloWorld, document.querySelector('#app'));
+class Hello extends React.Component
+{
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return <h1>Hello <span>{this.props.first[0].toUpperCase() + this.props.first.substr(1)}</span> <span className="red-text">{this.props.last.toUpperCase()}</span></h1>
+    }
+}
+
+function HelloWorld(props)
+{
+    return <h1>Hello <span>{props.first[0].toUpperCase() + props.first.substr(1)}</span> <span className="red-text">{props.last.toUpperCase()}</span></h1>
+    ;
+}
+
+ReactDOM.render(<Hello first={firstName} last={lastName} />, document.querySelector('#app'));
