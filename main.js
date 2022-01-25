79d9cce78916da7830a0f5d0cc237eeeccee99c4
BuildTools <unconfigured@null.spigotmc.org>
2022-01-25 15:12:13 +0100

    exo8 correct

--- main.js
+++ main.js
@@ -2,38 +2,74 @@ function UserGreeting(props) {
     return <h1>Bienvenue !</h1>;
   }
   
-  function GuestGreeting(props) {
-    return <h1>Veuillez vous connecter</h1>;
+function GuestGreeting(props) {
+return <h1>Veuillez vous connecter</h1>;
+}
+
+function UserGet(){
+
+}
+
+function Greeting(props) {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const [listItems, setListItems] = React.useState(() => {
+      fetch(`https://jsonplaceholder.typicode.com/users`)
+          .then(resp => resp.json()).then((data) => {
+          setListItems(data.map((user) =>
+              <tr key={user.id}>
+                  <td>{user.name}</td>
+                  <td>{user.phone}</td>
+                  <td>{user.email}</td>
+                  <td>{user.website}</td>
+                  <td>{user.company.name}</td>
+              </tr>
+          ));
+      })});
+
+  const handleLogIn = (e) => {
+      e.preventDefault();
+      setIsLoggedIn(true);
   }
-  function Greeting(props) {
-      const [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
-      const handleLogIn = (e) => {
-          e.preventDefault();
-          setIsLoggedIn(true);
-      }
+  const listUsers = () => {
+      return <table>
+          <thead>
+              <tr>
+                  <th>Nom</th>
+                  <th>Téléphone</th>
+                  <th>Email</th>
+                  <th>Site Web</th>
+                  <th>Nom de la compagnie</th>
+              </tr>
+          </thead>
+          <tbody>
+            {listItems}
+          </tbody>
+      </table>;
+  };
 
-      const handleLogOut = (e) => {
-          e.preventDefault();
-          setIsLoggedIn(false);
-      }
+  const handleLogOut = (e) => {
+      e.preventDefault();
+      setIsLoggedIn(false);
+  }
 
-    return(
+  return(
+    <React.Fragment>
+        {isLoggedIn ?
         <React.Fragment>
-            {isLoggedIn ? 
-            <React.Fragment>
-                <UserGreeting />
-                <button onClick={handleLogOut}>Se déconnecter</button>
-            </React.Fragment> : 
-            <React.Fragment>
-                <GuestGreeting />
-                <button onClick={handleLogIn}>Se connecter</button>
-            </React.Fragment> }
-        </React.Fragment>
-    )
-  }
+            <UserGreeting />
+            <button onClick={handleLogOut}>Se déconnecter</button>
+        </React.Fragment> :
+        <React.Fragment>
+            <GuestGreeting />
+            <button onClick={handleLogIn}>Se connecter</button>
+        </React.Fragment> }
+        {listUsers()}
+    </React.Fragment>
+  )
+}
   
-  ReactDOM.render(
+ReactDOM.render(
     <Greeting />,
     document.querySelector('#app')
-  );
+);
