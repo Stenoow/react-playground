feba71e11ce3fff96177e4b7323dc25d7a32bc34
BuildTools <unconfigured@null.spigotmc.org>
2022-01-25 15:47:26 +0100

    exo9 correct

--- main.js
+++ main.js
@@ -34,4 +34,56 @@ function UserCard({user}) {
     )
 }
 
-ReactDOM.render(<App/>, document.querySelector('#app'))
+function NameForm(props) {
+
+    const [state, setState] = React.useState({value: '', fruitz: 'coconut', textArea: 'Écrivez un essai à propos de votre élément du DOM préféré'});
+
+    const handleChange = (event)=> {
+        switch (event.target.name){
+            case 'value':
+                setState({value: event.target.value, fruitz: state.fruitz, textArea: state.textArea});
+                console.log(event.target.name)
+                break;
+            case 'textArea':
+                setState({textArea: event.target.value, value: state.value, fruitz: state.fruitz});
+                console.log(event.target.name)
+                break;
+            case 'fruitz':
+                setState({fruitz: event.target.value, value: state.value, textArea: state.textArea});
+                console.log(event.target.name)
+                break;
+        }
+
+    }
+
+    const handleSubmit = (event) => {
+        console.log(state)
+        alert('Le nom a été soumis : ' + state.value + ' / Le fruit préféré est : ' + state.fruitz + ' / Le textArea contient : ' + state.textArea);
+        event.preventDefault();
+    }
+
+    return (
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Nom :
+                    <input type="text" value={state.value} name="value" onChange={handleChange} />
+                </label>
+                <label>
+                    Essay:
+                    <textarea value={state.textArea} name="textArea" onChange={handleChange} />
+                </label>
+                <label>
+                    Choisissez votre parfum favori :
+                    <select value={state.fruitz} name="fruitz" onChange={handleChange}>
+                        <option value="grapefruit">Pamplemousse</option>
+                        <option value="lime">Citron vert</option>
+                        <option value="coconut">Noix de coco</option>
+                        <option value="mango">Mangue</option>
+                    </select>
+                </label>
+                <input type="submit" value="Envoyer" />
+            </form>
+        );
+}
+
+ReactDOM.render(<NameForm />, document.querySelector('#app'))
